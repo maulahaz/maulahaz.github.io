05963444dea27e236eb0d60f4347f62d79ee3468
maulahaz <51131133+maulahaz@users.noreply.github.com>
2020-05-02 04:22:21 +0400

    Install Apache di Laragon Portable

--- Catetan Web Developer.php
+++ Catetan Web Developer.php
@@ -21,3 +21,12 @@ Sumber: https://www.sitepoint.com/how-to-install-php-on-windows/
 Ada error [1115 - Unknown character set: 'utf8mb4'], solusinya:
 Ganti: mysql server 5.5.3 or newer, 
 Ato: replace all 'utf8mb4' to 'utf8' in your DB dump
+
+
+<!-- Install Apache di Laragon Portable -->
+<!-------------------------------------------------------------------->
+Laragon Portable atom Mint Edition, itu bundle nya pake Nginx.. krn biasanya pake Apache, gini cara jalaninnya:
+- Download Apache yg sesuai versi Windownya: https://www.apachelounge.com/download/ -- misalnya versi: httpd-2.4.28-win64-VC14
+- Extract the downloaded to: {LARAGON_ROOT}\bin\apache\httpd-2.4.28-win64-VC14
+- Jalanin Laragon, trus Select Menu > Apache > Version > httpd-2.4.28-win64-VC14
+- Jangan lupa Nginx nya di disable, di Menu Setting biar nggak bentrok.
